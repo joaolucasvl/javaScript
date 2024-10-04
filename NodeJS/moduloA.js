a352ab19d5a7b4ca733fce500766ea5ec413087f
joaolucasvl <joaovitorlucasdev@gmail.com>
2024-10-04 13:33:34 -0300

    Primeiro contato com node

---
+++ NodeJS/moduloA.js
@@ -0,0 +1,7 @@
+// Em node um arquivo representa um modulo
+
+// Como exportar um modulo no Node.JS
+
+this.ola = 'Fala Pessoal'
+exports.bemVindo = 'Bem vindo ao Node!'
+module.exports.ateLogo = 'Até próximo exemplo'
---
+++ NodeJS/moduloB.js
@@ -0,0 +1,8 @@
+// Modo comum de exportar modulos
+
+module.exports = {
+    bomDia: 'Bom dia',
+    boaNoite() {
+        return 'Boa noite'
+    }
+}
